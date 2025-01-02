14aa4646657088cb1d00a51dea4a7c0ae62742c3
Gustavo <gustavo.brancaglione@gmail.com>
2025-01-02 18:08:07 -0300

    Fix rubocop

--- app/controllers/backoffice/application_controller.rb
+++ app/controllers/backoffice/application_controller.rb
@@ -1,6 +1,6 @@
 module Backoffice
   class ApplicationController < ActionController::Base
-    layout 'backoffice/application'
+    layout "backoffice/application"
 
     include Authentication
     # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
